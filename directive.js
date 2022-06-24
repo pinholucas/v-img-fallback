9552d7c6879bf053220c6e7a782eed75edd4b1b8
Lucas Pinho B. Santos <5108881+pinholucas@users.noreply.github.com>
2022-06-24 06:29:53 +0000

    chore: update custom directives for vue3

--- directive.js
+++ directive.js
@@ -1,8 +1,8 @@
 export default {
-  bind (el, binding) {
+  beforeMount(el, binding) {
     const { value } = binding;
-    const defaultLoading = 'http://de.4-traders.com/images/loading_100.gif';
-    const defaultError = 'https://pbs.twimg.com/media/BXhh-sfIAAArh4S.jpg';
+    const defaultLoading = "http://de.4-traders.com/images/loading_100.gif";
+    const defaultError = "https://pbs.twimg.com/media/BXhh-sfIAAArh4S.jpg";
     const img = new Image();
 
     let loading = defaultLoading;
@@ -15,7 +15,7 @@ export default {
       );
     }
 
-    if (typeof value === 'string') {
+    if (typeof value === "string") {
       loading = value;
       error = value;
     }
@@ -36,5 +36,5 @@ export default {
     img.onerror = () => {
       el.src = error;
     };
-  }
+  },
 };
